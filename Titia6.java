f08a1911b159aa1e7ba67b138efc58f900d6ef14
Yukios7 <fabioyukio123456@gmail.com>
2019-06-18 13:47:27 -0300

    sem tempo irmao

---
+++ Titia6.java
@@ -0,0 +1,21 @@
+package EDNA6;
+
+import java.util.Scanner;
+
+public class Titia6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner omg = new Scanner(System.in);
+		int n = omg.nextInt();
+
+		if ((n % 2)== 0) {
+			System.out.println("O número é par");
+		}
+		else {
+			System.out.println("O número é impar");
+		}
+		
+	}
+
+}
